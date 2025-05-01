c91038a83baa602067df83c4af23fe7d7cdff9e6
ebgarcia <ebgarcia@sudamericano.edu.ec>
2025-05-01 16:50:22 -0500

    update

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,6 +3,14 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
+  career: any;
+  enrollment: any;
+  subject: any;
+  assignment: any;
+  courseRecord: any;
+  registration: any;
+  student: any;
+  teacher: any;
   constructor() {
     super();
   }
